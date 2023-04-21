896a52172f86adb4eb638c4239ab2c7b72606a7a
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-21 10:38:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
 
-
+	let strongList = document.querySelectorAll('strong');
+	strongList.forEach((element) => {
+		element.style.color = 'green'
+	})
 }
 
 
 function return_normal() {
     //Write your code here
 
-    
+    let strongList = document.querySelectorAll('strong');
+	strongList.forEach((element) => {
+		element.style.color = 'black'
+	})
 }
